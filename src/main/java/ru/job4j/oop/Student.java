f8e119213c4cc5899ec67494b246fb5b2272c94b
Oleg <korotkiy.o@yandex.ru>
2021-09-02 16:56:17 +0300

    После проверки Checkstyle

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -6,12 +6,12 @@ public class Student {
     }
 
     public void music(String lirics) {
-        System.out.println("I can sign a song :"  +lirics);
+        System.out.println("I can sign a song :"  + lirics);
     }
 
     public static void main(String[] args) {
         Student petya = new Student();
-        String song =" I believe I can fly";
+        String song = " I believe I can fly";
         petya.music(song);
     }
 
