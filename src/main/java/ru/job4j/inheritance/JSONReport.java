114038ac4220c7d6b07672219caa7272bd64f00d
Oleg <korotkiy.o@yandex.ru>
2021-09-08 09:35:24 +0300

    4. Переопределение [#174890]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,5 +1,8 @@
 package ru.job4j.inheritance;
 
-public class JSONReport extends TextReport{
+public class JSONReport extends TextReport {
 
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator() + "   "  + name + " " + ":" + " " + name + "," + System.lineSeparator() + "   " + body + " " + ":" + " " + body + System.lineSeparator() + "}";
+    }
 }
--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -3,6 +3,6 @@ package ru.job4j.inheritance;
 public class TextReport {
 
     public String generate(String name, String body) {
-        return "{" + System.lineSeparator() + "   "  + name + " " + ":" + " " + name + "," + System.lineSeparator() + "   " + body + " " + ":" + " " + body + System.lineSeparator() + "}";
+        return name + System.lineSeparator() + body;
     }
 }
