6b20b53e8202a62b0180bb4e251ec8ad98458abc
Oleg <korotkiy.o@yandex.ru>
2021-09-08 10:03:44 +0300

    Удаление профессий

--- src/main/java/ru/job4j/oop/Builder.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.oop;
-
-public class Builder extends Engineer {
-    private String razrayd;
-    private String vidRabot;
-
-    public String kladkaSten(Brick brick) {}
-
-}
--- src/main/java/ru/job4j/oop/Dentist.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.oop;
-
-public class Dentist extends Doctor {
-
-    private String department;
-    private String qualification;
-
-    public String operation(Pacient pacient) {}
-}
