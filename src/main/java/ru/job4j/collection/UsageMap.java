d74efce42271e29456fb770553c59038946d2e79
Thinkpad <korotkiy.o@yandex.ru>
2021-10-23 13:09:26 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,17 +6,10 @@ import java.util.Map;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("oleg@mail.ru", "Oleg Korotkiy");
-        map.put("oleg@yandex.ru", "Oleg Korotkiy");
-        map.put("oleg@yandex.ru", "Oleg ");
-        map.remove("oleg@yandex.ru");
+        map.put("korokity.o@mail.ru", "Korotkiy O.Y.");
+        map.put("1swk3@andex.ru", "Tarasov I.K.");
         for(String key : map.keySet()) {
             String value = map.get(key);
-            System.out.println(key + " = " + value);
-        }
-        for(Map.Entry<String, String> entry : map.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
             System.out.println(key + " = " + value);
         }
     }
