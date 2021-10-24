fc0589979f3b8ba7174702a60801b4f5398af64c
Thinkpad <korotkiy.o@yandex.ru>
2021-10-24 19:52:30 +0300

    2. Преобразования List в Map. [#10093 #219789]

--- src/main/java/ru/job4j/collection/OrderConvert.java
+++ src/main/java/ru/job4j/collection/OrderConvert.java
@@ -1,9 +1,11 @@
 package ru.job4j.collection;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class OrderConvert {
+
     public static HashMap<String, Order> process(List<Order> orders) {
         HashMap<String, Order> map = new HashMap<>();
         for (Order index : orders) {
--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("korokity.o@mail.ru", "Korotkiy O.Y.");
+        map.put("korokity.o@mail.ru", "Korotkiy O.Y.");
+        map.put("korokity.o@mail.ru", "Korotkiy O.Y.");
         map.put("1swk3@andex.ru", "Tarasov I.K.");
         for(String key : map.keySet()) {
             String value = map.get(key);
