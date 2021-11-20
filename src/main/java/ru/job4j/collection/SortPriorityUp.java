7640a220d57be9c16927598fa51d1430d675c11d
Thinkpad <korotkiy.o@yandex.ru>
2021-11-20 17:11:24 +0300

    1.3. Встроенные функциональные интерфейсы. [#249209]

--- src/main/java/ru/job4j/collection/SortPriorityUp.java
+++ src/main/java/ru/job4j/collection/SortPriorityUp.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 public class SortPriorityUp implements Comparator<Job> {
     @Override
     public int compare(Job o1, Job o2) {
+
         return Integer.compare(o1.getPriority(), o2.getPriority());
     }
 }
