38340ee3a2221fa13a497cb0368b370d7d836865
Thinkpad <korotkiy.o@yandex.ru>
2021-11-25 22:37:26 +0300

    2. Список адресов [#110062]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -4,13 +4,28 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ProfilesTest {
 
     @Test
     public void collect() {
-        List<Profile> = List.of()
+        Address addressOne = new Address("Moscow", "Lenina", 10, 5);
+        Address addressTwo = new Address("Moscow", "Lenina", 2, 9);
+        Address addressTree = new Address("Moscow", "Lenina", 6, 5);
+
+        List<Address> expected = List.of(addressOne, addressTwo, addressTree);
+
+        List<Profile> profilesList = List.of(
+                new Profile(addressOne),
+                new Profile(addressTwo),
+                new Profile(addressTree)
+        );
+
+        Profiles profiles = new Profiles();
+
+        assertThat(profiles.collect(profilesList), is(expected));
 
     }
 }
