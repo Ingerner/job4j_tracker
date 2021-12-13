894997b5a7f63d2cf436945d345412b75b9ff656
Thinkpad <korotkiy.o@yandex.ru>
2021-12-13 23:29:40 +0300

    1.9. Статические и не статические методы [#504806]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Analyze {
@@ -11,6 +12,21 @@ public class Analyze {
     }
 
     public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
-        return stream.map(pupil -> new Tuple(pupil.getName(),                      ));
+        return stream
+                .map(
+                        pupil ->
+                                new Tuple(
+                                        pupil.getName(),
+                                        pupil
+                                                .getSubjects()
+                                                .stream()
+                                                .mapToInt(Subject::getScore)
+                                                .average().orElse(0D)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.flatMap(subject -> subject.getSubjects().stream())
+                .collect(Collectors.groupingBy( )).
     }
 }
