1f464fac728e982c2dc1af055f278cc6808d63bb
Oleg <korotkiy.o@yandex.ru>
2022-09-07 16:37:14 +0300

    4. Аттестация [#504878]

--- src/main/java/ru/job4j/map/AnalyzeByMap.java
+++ src/main/java/ru/job4j/map/AnalyzeByMap.java
@@ -32,7 +32,7 @@ public class AnalyzeByMap {
             for (Subject s: listSubject) {
                 sumSt += s.getScore();
             }
-            rsl.add(new Label(p.name(), sumSt/listSubject.size()));
+            rsl.add(new Label(p.name(), sumSt / listSubject.size()));
             sumSt = 0;
         }
         return rsl;
@@ -51,7 +51,7 @@ public class AnalyzeByMap {
         }
         List<Label> list = new ArrayList<>();
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            list.add(new Label(entry.getKey(), entry.getValue()/pupils.size()));
+            list.add(new Label(entry.getKey(), entry.getValue() / pupils.size()));
         }
         return list;
     }
@@ -70,9 +70,8 @@ public class AnalyzeByMap {
             sumSt = 0;
         }
             Collections.sort(rsl);
-        return rsl.get(rsl.size()-1);
+        return rsl.get(rsl.size() - 1);
     }
-
 
     public static Label bestSubject(List<Pupil> pupils) {
         Map<String, Integer> map = new LinkedHashMap<>();
