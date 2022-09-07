e6dfeb6190a47e700c3e2a3db71fa6cd45585bea
Oleg <korotkiy.o@yandex.ru>
2022-09-07 16:33:26 +0300

    4. Аттестация [#504878]

--- src/main/java/ru/job4j/map/AnalyzeByMap.java
+++ src/main/java/ru/job4j/map/AnalyzeByMap.java
@@ -1,12 +1,8 @@
 package ru.job4j.map;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AnalyzeByMap {
-    //*вычисляет общий средний балл*/
     public static double averageScore(List<Pupil> pupils) {
         List<Subject> listSubject = new ArrayList<>();
         double sumStudnts = 0;
@@ -26,7 +22,6 @@ public class AnalyzeByMap {
         return sumAvg / pupils.size();
     }
 
-    //*вычисляет средний балл по каждому ученику*/
     public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
         double sumSt = 0;
         List<Subject> listSubject = new ArrayList<>();
@@ -43,7 +38,6 @@ public class AnalyzeByMap {
         return rsl;
     }
 
-    //*вычисляет средний балл по каждому предмету//
     public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
         Map<String, Integer> map = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
@@ -59,16 +53,45 @@ public class AnalyzeByMap {
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             list.add(new Label(entry.getKey(), entry.getValue()/pupils.size()));
         }
-
         return list;
     }
 
     public static Label bestStudent(List<Pupil> pupils) {
-        return null;
+        double sumSt = 0;
+        List<Subject> listSubject = new ArrayList<>();
+        List<Label> rsl = new ArrayList<>();
+        int i = 0;
+        for (Pupil p : pupils) {
+            listSubject = p.subjects();
+            for (Subject s: listSubject) {
+                sumSt += s.getScore();
+            }
+            rsl.add(new Label(p.name(), sumSt));
+            sumSt = 0;
+        }
+            Collections.sort(rsl);
+        return rsl.get(rsl.size()-1);
     }
 
+
     public static Label bestSubject(List<Pupil> pupils) {
-        return null;
+        Map<String, Integer> map = new LinkedHashMap<>();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                int score = subject.getScore();
+                if (map.containsKey(subject.getName())) {
+                    score = map.get(subject.getName()) + score;
+                }
+                map.put(subject.getName(), score);
+            }
+        }
+        List<Label> listLabel = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            listLabel.add(new Label(entry.getKey(), entry.getValue()));
+        }
+        Collections.sort(listLabel);
+        return listLabel.get(listLabel.size() - 1);
+
     }
 
 }
